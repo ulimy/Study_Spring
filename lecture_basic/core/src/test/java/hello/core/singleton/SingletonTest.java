731cd6078cb057264a4cbcbecfe4cfdc30eabf9b
ulimy <ulim130@gmail.com>
2022-03-07 19:36:29 +0900

    싱글톤 패턴

---
+++ lecture_basic/core/src/test/java/hello/core/singleton/SingletonTest.java
@@ -0,0 +1,17 @@
+package hello.core.singleton;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SingletonTest {
+    @Test
+    @DisplayName("싱글톤 패턴을 적용한 객체 사용")
+    void singletonServiceTest() {
+        SingletonService singletonService1 = SingletonService.getInstance();
+        SingletonService singletonService2 = SingletonService.getInstance();
+
+        assertThat(singletonService1).isSameAs(singletonService2);
+    }
+}
